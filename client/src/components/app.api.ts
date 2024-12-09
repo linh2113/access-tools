01c0503c0c92960a0830b02eaf62b637dfdc1892
linh2113 <21130091@st.hcmuaf.edu.vn>
2024-12-09 23:32:21 +0700

    fix bug

--- client/src/components/app.api.ts
+++ client/src/components/app.api.ts
@@ -2,13 +2,9 @@ import http from '@/lib/http'
 import { ProductType } from '@/types/type'
 // 5. Call API từ server
 export const getProducts = () => http.get<ProductType[]>('/products')
-
 export const getTotalProducts = () => http.get('/products/count')
 export const getAveragePrice = () => http.get('/products/average-price')
 export const getPriceRange = () => http.get('/products/price-range')
-
 export const getWeight = () => http.get('/products/weight-stats')
-
 export const getManufacturer = () => http.get('/products/manufacturer-stats')
-
 export const getResolution = () => http.get('/products/resolution-stats')
--- client/src/components/statistics.tsx
+++ client/src/components/statistics.tsx
@@ -15,6 +15,7 @@ import { Bar } from 'react-chartjs-2'
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, ArcElement } from 'chart.js'
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, ArcElement)
+// 6. Hiển thị data lên UI
 export default function Statistics() {
    const totalProduct = useQuery({
       queryKey: ['totalProduct'],
